7cd9882197dd07dff82b33ce8f1a56b72d3e582b
KarlDragon <giocodeproject@gmail.com>
2026-06-09 20:16:50 +0700

    Create db table

---
+++ BE/Models/Users.cs
@@ -0,0 +1,10 @@
+public class Users
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string PasswordHash { get; set; } = "";
+    public string Role { get; set; } = "";
+    public string Name { get; set; } = "";
+}
+
