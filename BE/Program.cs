fc2b141b833d099f422a7317018f03133e2bfbf0
KarlDragon <giocodeproject@gmail.com>
2026-06-09 23:40:16 +0700

    Refactor Program.cs: organize using directives and add IAuthRepository registration

--- BE/Program.cs
+++ BE/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
-
+using BE.Repositories.Interfaces;
+using BE.Repositories.Implementations;
 var builder = WebApplication.CreateBuilder(args);
 
 var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -9,7 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(ConnectionString, new MySqlServerVersion(new Version(8, 0, 44))));
 
 builder.Services.AddControllers();
-
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
