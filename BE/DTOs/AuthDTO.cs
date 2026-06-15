112c98178f12a93f256f3a1c250880b13b1b1eff
KarlDragon <giocodeproject@gmail.com>
2026-06-15 08:18:33 +0700

    Add AuthDTO classes for user registration and login data transfer

---
+++ BE/DTOs/AuthDTO.cs
@@ -0,0 +1,16 @@
+namespace BE.DTOs;
+
+public class RegisterDTO
+{
+    public string Username { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string Role { get; set; } = "";
+    public string Name { get; set; } = "";
+}
+
+public class LoginDTO
+{
+    public string Email { get; set; } = "";
+    public string PasswordHash { get; set; } = "";
+}
