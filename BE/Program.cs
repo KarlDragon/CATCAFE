91ed3a7555988a8317c8a275c985691b4a33bb09
KarlDragon <giocodeproject@gmail.com>
2026-06-15 08:18:22 +0700

    Refactor Program.cs: add FluentValidation and Redis connection setup, register services

--- BE/Program.cs
+++ BE/Program.cs
@@ -1,8 +1,19 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using FluentValidation;
 using BE.Repositories.Interfaces;
 using BE.Repositories.Implementations;
-var builder = WebApplication.CreateBuilder(args);
+using BE.Services.Interfaces;
+using BE.Services.Implementations;
+using StackExchange.Redis;
 
+var builder = WebApplication.CreateBuilder(args);
+var redisConnection = builder.Configuration.GetConnectionString("RedisConnection");
+if (string.IsNullOrEmpty(redisConnection))
+{
+    throw new Exception("Redis connection string is not configured.");
+}
+var redis = ConnectionMultiplexer.Connect(redisConnection);
 var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add dbcontext to the services container
@@ -10,7 +21,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(ConnectionString, new MySqlServerVersion(new Version(8, 0, 44))));
 
 builder.Services.AddControllers();
+
+builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+builder.Services.AddSingleton<IConnectionMultiplexer>(redis);
+
+// Scoped repositories
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+
+// Scoped services
+builder.Services.AddScoped<IRegistrationFilterService, RegistrationFilterService>();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
