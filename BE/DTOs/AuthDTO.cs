224e325661b3f650c96f064f5124a4ebf761b565
KarlDragon <giocodeproject@gmail.com>
2026-06-16 18:29:26 +0700

    Add RefreshToken property to AuthResponseDTO for enhanced authentication handling

--- BE/DTOs/AuthDTO.cs
+++ BE/DTOs/AuthDTO.cs
@@ -18,6 +18,7 @@ public class LoginDTO
 public class AuthResponseDTO
 {
     public string Token { get; set; } = "";
+    public string RefreshToken { get; set;} = "";
     public string Username { get; set; } = "";
     public string Email { get; set; } = "";
     public string Role { get; set; } = "";
