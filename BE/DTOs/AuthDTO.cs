2702715a0822d2d09585c2eeed83d63dbfa81755
KarlDragon <giocodeproject@gmail.com>
2026-06-16 17:49:43 +0700

    Add AuthResponseDTO class for handling authentication responses

--- BE/DTOs/AuthDTO.cs
+++ BE/DTOs/AuthDTO.cs
@@ -14,3 +14,12 @@ public class LoginDTO
     public string EmailOrUsername { get; set; } = "";
     public string Password { get; set; } = "";
 }
+
+public class AuthResponseDTO
+{
+    public string Token { get; set; } = "";
+    public string Username { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Role { get; set; } = "";
+    public string Name { get; set; } = "";
+}
