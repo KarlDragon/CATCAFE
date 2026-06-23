8e7463a348e79a26eba0c883afe1c30c31ad2382
KarlDragon <giocodeproject@gmail.com>
2026-06-23 23:12:19 +0700

    Add constructor to AppDbContext for dependency injection

--- BE/DBContext/AppDbContext.cs
+++ BE/DBContext/AppDbContext.cs
@@ -3,6 +3,7 @@ using BE.Models;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
     public DbSet<Users> Users { get; set; }
     public DbSet<RefreshToken> RefreshTokens { get; set; }
     public DbSet<Table> Tables { get; set; }
