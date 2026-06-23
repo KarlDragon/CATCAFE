292982cc543eea377974b2ba194275aa39b5be0a
KarlDragon <giocodeproject@gmail.com>
2026-06-23 16:41:11 +0700

    Add DbSet for Table in AppDbContext to support table management

--- BE/DBContext/AppDbContext.cs
+++ BE/DBContext/AppDbContext.cs
@@ -9,4 +9,5 @@ public class AppDbContext : DbContext
 
     public DbSet<Users> Users { get; set; }
     public DbSet<RefreshToken> RefreshTokens { get; set; }
+    public DbSet<Table> Tables { get; set; }
 }
