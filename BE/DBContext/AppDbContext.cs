da6cbc277e7ac66fd2b5749c67329d70c4718065
KarlDragon <giocodeproject@gmail.com>
2026-06-23 21:57:38 +0700

    Remove Table entity and associated migration; update model snapshot accordingly

--- BE/DBContext/AppDbContext.cs
+++ BE/DBContext/AppDbContext.cs
@@ -9,7 +9,6 @@ public class AppDbContext : DbContext
 
     public DbSet<Users> Users { get; set; }
     public DbSet<RefreshToken> RefreshTokens { get; set; }
-    public DbSet<Table> Tables { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
