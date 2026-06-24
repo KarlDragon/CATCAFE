466f881143d3edd386e59c7ab73647e3926487b1
KarlDragon <giocodeproject@gmail.com>
2026-06-24 08:57:00 +0700

    Add UpdateFoodDrinkDTO class for food and drink update data transfer

--- BE/DTOs/UpdateDataDTO.cs
+++ BE/DTOs/UpdateDataDTO.cs
@@ -15,3 +15,11 @@ public class UpdateCatDTO
     public string? Status {get; set;} = "";
     
 }
+
+public class UpdateFoodDrinkDTO
+{
+    public int FoodDrinkID {get; set;}
+    public string? Name {get; set;} = "";
+    public decimal? Price {get; set;}
+    public int? Quantity {get; set;}
+}
