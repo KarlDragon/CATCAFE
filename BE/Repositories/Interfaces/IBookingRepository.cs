87301490c8299cb3f030c319305d7aaa111d2d3a
KarlDragon <giocodeproject@gmail.com>
2026-06-29 17:55:02 +0700

    Add using directive for BE.Models in IBookingRepository interface

--- BE/Repositories/Interfaces/IBookingRepository.cs
+++ BE/Repositories/Interfaces/IBookingRepository.cs
@@ -1,6 +1,10 @@
 namespace BE.Repositories.Interfaces;
-
+using BE.Models;
 public interface IBookingRepository
 {
-    
+    Task<bool> CreateBookingAsync( Booking booking );
+
+    Task<bool> ChangeBookingStatusAsync( int bookingId );
+
+    Task<IEnumerable<Booking>> GetBookingsAsync();
 }
