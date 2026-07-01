a2fba9997f48282f6d64adf7741ef2c78bc5b415
KarlDragon <giocodeproject@gmail.com>
2026-07-01 08:59:00 +0700

    Add CreateFoodDrinkDTO class for food and drink data representation

--- BE/DTOs/CreateDataDTO.cs
+++ BE/DTOs/CreateDataDTO.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BE.DTOs;
 
 public class CreateTableDTO
@@ -13,4 +15,13 @@ public class CreateCatDTO
     public string Status {get; set;} = "";
     public string Description {get; set;} = "";
 
+}
+
+public class CreateFoodDrinkDTO
+{
+    public string Name {get; set;} = "";
+    
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price {get; set;}
+    public int Quantity {get; set;}
 }
