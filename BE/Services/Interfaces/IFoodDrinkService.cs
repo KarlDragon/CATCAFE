7216253ca2e340002122380fbe6defc7e1992201
KarlDragon <giocodeproject@gmail.com>
2026-07-01 08:59:11 +0700

    Add IFoodDrinkService interface for food and drink operations

---
+++ BE/Services/Interfaces/IFoodDrinkService.cs
@@ -0,0 +1,14 @@
+namespace BE.Services.Interfaces;
+using BE.Models;
+using BE.DTOs;
+
+public interface IFoodDrinkService
+{
+    Task<bool> CreateFoodDrinkAsync( FoodDrink foodDrink );
+
+    Task RemoveFoodDrinkAsync( int foodDrinkId );
+
+    Task<bool> UpdateFoodDrinkAsync( UpdateFoodDrinkDTO updateFoodDrinkDTO );
+
+    Task<IEnumerable<FoodDrink>> GetAllFoodDrinksAsync(CancellationToken cancellationToken);
+}
