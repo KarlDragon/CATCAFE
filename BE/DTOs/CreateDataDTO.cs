dbaba2849a36f5b906db66a2e43eaff59c5b8068
KarlDragon <giocodeproject@gmail.com>
2026-07-01 07:32:42 +0700

    Add CreateCatDTO for cat creation data transfer

--- BE/DTOs/CreateDataDTO.cs
+++ BE/DTOs/CreateDataDTO.cs
@@ -5,3 +5,12 @@ public class CreateTableDTO
     public string TableName {get; set;} = "";
     public int SeatAmount {get; set;}
 }
+
+public class CreateCatDTO
+{
+    public string CatName {get; set;} = "";
+    public string Breed {get; set;} = "";
+    public string Status {get; set;} = "";
+    public string Description {get; set;} = "";
+
+}
