a2cfd0bc71586e07f0d4818025c0c15167e1c9a3
KarlDragon <giocodeproject@gmail.com>
2026-07-02 21:46:58 +0700

    Implement CatController with CRUD operations for managing cats

---
+++ BE/Controllers/CatController.cs
@@ -0,0 +1,58 @@
+namespace BE.Controllers;
+using BE.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using BE.DTOs;
+using Microsoft.AspNetCore.Authorization;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class CatController : ControllerBase
+{
+    private readonly ICatService _catService;
+
+    public CatController(ICatService catService)
+    {
+        _catService = catService;
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Owner")]
+    public async Task<IActionResult> CreateCat([FromBody] CreateCatDTO createCatDTO)
+    {
+        bool isCreated = await _catService.CreateCatAsync(createCatDTO);
+        if (!isCreated)
+        {
+            return BadRequest(new { message = "Failed to create cat." });
+        }
+        return Ok(new { message = "Cat created successfully." });
+    }
+
+    [HttpDelete("{catId}")]
+    [Authorize(Roles = "Owner")]
+    public async Task<IActionResult> RemoveCat(int catId)
+    {
+        await _catService.RemoveCatAsync(catId);
+        return Ok(new { message = "Cat removed successfully." });
+    }
+
+    [HttpPut]
+    [Authorize(Roles = "Owner, Staff")]
+    public async Task<IActionResult> UpdateCat([FromBody] UpdateCatDTO updateCatDTO)
+    {
+        bool isUpdated = await _catService.UpdateCatAsync(updateCatDTO);
+        if (!isUpdated)
+        {
+            return BadRequest(new { message = "Failed to update cat." });
+        }
+        return Ok(new { message = "Cat updated successfully." });
+    }
+
+    [HttpGet]
+    [Authorize]
+    public async Task<IActionResult> GetAllCats(CancellationToken cancellationToken)
+    {
+        var cats = await _catService.GetAllCatsAsync(cancellationToken);
+        return Ok(cats);
+    }
+}
