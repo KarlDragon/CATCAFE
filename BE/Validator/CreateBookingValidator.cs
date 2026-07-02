2a7ae3ab2fb20ccf27adf8231de84531d1d4281a
KarlDragon <giocodeproject@gmail.com>
2026-07-02 17:13:00 +0700

    Add CreateBookingValidator for validating CreateBookingDTO properties

---
+++ BE/Validator/CreateBookingValidator.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+using BE.DTOs;
+public class CreateBookingValidator : AbstractValidator<CreateBookingDTO>
+{
+    public CreateBookingValidator()
+    {
+        RuleFor(x => x.TableID)
+            .NotEmpty().WithMessage("Không được để trống.")
+            .GreaterThan(0).WithMessage("ID bàn phải lớn hơn 0.");
+
+        RuleFor(x => x.UserID)
+            .NotEmpty().WithMessage("Không được để trống.")
+            .GreaterThan(0).WithMessage("ID người dùng phải lớn hơn 0.");
+
+        RuleFor(x => x.BookedTime)
+            .NotEmpty().WithMessage("Không được để trống.")
+            .GreaterThan(DateTime.Now).WithMessage("Thời gian đặt phải lớn hơn thời gian hiện tại.");
+
+        RuleFor(x => x.EndTime)
+            .NotEmpty().WithMessage("Không được để trống.")
+            .GreaterThan(x => x.BookedTime).WithMessage("Thời gian kết thúc phải lớn hơn thời gian đặt.");
+    }
+    
+}
