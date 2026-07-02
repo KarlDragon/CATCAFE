20b7c1b1d33a5b71a74ae9632ecbc110cdaf6a49
KarlDragon <giocodeproject@gmail.com>
2026-07-02 17:15:15 +0700

    Add CreateBookingDetailValidator for validating CreateBookingDetailDTO properties

---
+++ BE/Validator/CreateBookingDetailValidator.cs
@@ -0,0 +1,19 @@
+using FluentValidation;
+using BE.DTOs;
+public class CreateBookingDetailValidator : AbstractValidator<CreateBookingDetailDTO>
+{
+    public CreateBookingDetailValidator()
+    {
+        RuleFor(x => x.FoodDrinkID)
+            .NotEmpty().WithMessage("Không được để trống.")
+            .GreaterThan(0).WithMessage("ID món ăn/đồ uống phải lớn hơn 0.");
+
+        RuleFor(x => x.Quantity)
+            .NotEmpty().WithMessage("Không được để trống.")
+            .GreaterThan(0).WithMessage("Số lượng phải lớn hơn 0.");
+
+        RuleFor(x => x.PriceAtBooking)
+            .NotEmpty().WithMessage("Không được để trống.")
+            .GreaterThan(0).WithMessage("Giá tại thời điểm đặt phải lớn hơn 0.");
+    }
+}
