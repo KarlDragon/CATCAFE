0b7c715565edfbaf394da387682e30feb547aeda
KarlDragon <giocodeproject@gmail.com>
2026-07-02 21:34:34 +0700

    Remove ValidateTempTokenAsync method from IJwtService and JwtService

--- BE/Services/Interfaces/IJwtService.cs
+++ BE/Services/Interfaces/IJwtService.cs
@@ -5,6 +5,5 @@ public interface IJwtService
     public string GenerateToken(Users users);
     public string GenerateRefreshToken();
     public Task<bool> ValidateRefreshTokenAsync(int userID, string refreshTokenDTO);
-    public Task<bool> ValidateTempTokenAsync( string token );
 }
 
