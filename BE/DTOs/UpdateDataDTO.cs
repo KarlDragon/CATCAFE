1a1c41185fc12446df9a0606b190bcd8943e94a6
KarlDragon <giocodeproject@gmail.com>
2026-07-02 22:16:56 +0700

    Remove default values from properties in UpdateDataDTO classes

--- BE/DTOs/UpdateDataDTO.cs
+++ BE/DTOs/UpdateDataDTO.cs
@@ -3,23 +3,23 @@ namespace BE.DTOs;
 public class UpdateTableDTO
 {
     public int TableID { get; set; }
-    public string? TableName { get; set; } = "";
+    public string? TableName { get; set; }
     public int? SeatAmount { get; set; }
 }
 
 public class UpdateCatDTO
 {
     public int CatID {get; set;}
-    public string? CatName {get; set;} = "";
-    public string? Breed {get; set;} = "";
-    public string? Status {get; set;} = "";
-    public string? Description {get; set;} = "";
+    public string? CatName {get; set;}
+    public string? Breed {get; set;}
+    public string? Status {get; set;}
+    public string? Description {get; set;}
 }
 
 public class UpdateFoodDrinkDTO
 {
     public int FoodDrinkID {get; set;}
-    public string? Name {get; set;} = "";
+    public string? Name {get; set;}
     public decimal? Price {get; set;}
     public int? Quantity {get; set;}
 }
