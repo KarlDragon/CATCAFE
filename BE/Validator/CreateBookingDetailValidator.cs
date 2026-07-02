f8b5e8afe1bdfbaee158fc0a13ac1818b60b04d4
KarlDragon <giocodeproject@gmail.com>
2026-07-02 22:13:20 +0700

    Refactor CreateBookingDetailValidator to remove price validation rule

--- BE/Validator/CreateBookingDetailValidator.cs
+++ BE/Validator/CreateBookingDetailValidator.cs
@@ -13,7 +13,6 @@ public class CreateBookingDetailValidator : AbstractValidator<CreateBookingDetai
             .GreaterThan(0).WithMessage("Số lượng phải lớn hơn 0.");
 
         RuleFor(x => x.PriceAtBooking)
-            .NotEmpty().WithMessage("Không được để trống.")
-            .GreaterThan(0).WithMessage("Giá tại thời điểm đặt phải lớn hơn 0.");
+            .NotEmpty().WithMessage("Không được để trống.");
     }
 }
