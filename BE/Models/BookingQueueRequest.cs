70cbf816034150c190c9655b2b6453dff39d9407
KarlDragon <giocodeproject@gmail.com>
2026-07-07 02:05:12 +0700

    Add BookingQueueRequest and BookingResult models for booking processing

--- BE/Models/BookingQueueRequest.cs
+++ BE/Models/BookingQueueRequest.cs
@@ -1,9 +1,9 @@
-namespace BE.Infrastructure.Queue;
-
+namespace BE.Models;
 using BE.DTOs;
 
 public class BookingQueueRequest
 {
     public CreateBookingDTO CreateBookingDTO { get; set; } = null!;
     public int UserId { get; set; }
+    public TaskCompletionSource<bool> CompletionSource { get; set; } = new TaskCompletionSource<bool>();
 }
---
+++ BE/Models/BookingResult.cs
@@ -0,0 +1,7 @@
+namespace BE.Models;
+
+public class BookingResult
+{
+    public int BookingId  { get; set; }
+    public BookingStatus Status  { get; set; }
+}
