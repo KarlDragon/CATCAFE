497e868db56921f359a235701e20b976cf6a750f
KarlDragon <giocodeproject@gmail.com>
2026-07-08 08:28:54 +0700

    Remove Role property from RegisterDTO class

--- BE/DTOs/AuthDTO.cs
+++ BE/DTOs/AuthDTO.cs
@@ -5,7 +5,6 @@ public class RegisterDTO
     public string Username { get; set; } = "";
     public string Email { get; set; } = "";
     public string Password { get; set; } = "";
-    public string Role { get; set; } = "";
     public string Name { get; set; } = "";
 }
 
