85b715f27e0999e0172d8d0f83b0ad45d2929da0
KarlDragon <giocodeproject@gmail.com>
2026-07-08 07:48:38 +0700

    Add cancellation support to EnqueueAsync method in IRequestQueue and RequestQueue

--- BE/Infrastructure/Queues/IRequestQueue.cs
+++ BE/Infrastructure/Queues/IRequestQueue.cs
@@ -1,6 +1,10 @@
+using System.Threading.Channels;
+
 namespace BE.Infrastructure.Queue;
 public interface IRequestQueue<T>
 {
-    ValueTask EnqueueAsync(T item);
+    ValueTask EnqueueAsync(T item, CancellationToken cancellationToken);
     ValueTask<T> DequeueAsync(CancellationToken cancellationToken);
+
+    ChannelReader<T> Reader { get; }
 }
--- BE/Infrastructure/Queues/RequestQueue.cs
+++ BE/Infrastructure/Queues/RequestQueue.cs
@@ -13,17 +13,19 @@ public class RequestQueue<T> : IRequestQueue<T>
         _channel = Channel.CreateBounded<T>(options);
     }
 
-    public async ValueTask EnqueueAsync(T item)
+    public async ValueTask EnqueueAsync(T item, CancellationToken cancellationToken)
     {
         if (item == null)
         {
             throw new ArgumentNullException(nameof(item), "Cannot enqueue a null item.");
         }
-        await _channel.Writer.WriteAsync(item);
+        await _channel.Writer.WriteAsync(item, cancellationToken);
     }
 
     public async ValueTask<T> DequeueAsync(CancellationToken cancellationToken)
     {
         return await _channel.Reader.ReadAsync(cancellationToken);
     }
+
+    public ChannelReader<T> Reader => _channel.Reader;
 }
