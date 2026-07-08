6c5e8e1c0c5743020a25326d9e41f23e741c5099
KarlDragon <giocodeproject@gmail.com>
2026-07-08 07:48:21 +0700

    Add SendWelcomeEmailAsync method to INotificationService interface

--- BE/Services/Interfaces/INotificationService.cs
+++ BE/Services/Interfaces/INotificationService.cs
@@ -3,5 +3,6 @@ namespace BE.Services.Interfaces
     public interface INotificationService
     {
         Task SendEmailAsync(string toUserName, string toEmail, string subject, string body, CancellationToken cancellationToken = default);
+        Task SendWelcomeEmailAsync(string toUserName, string toEmail, CancellationToken cancellationToken = default);
     }
 }
