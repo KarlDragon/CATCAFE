6525199ca80e6f7de9a46c03f9f0e28b4a438590
KarlDragon <giocodeproject@gmail.com>
2026-08-12 11:20:02 +0700

    Refactor CreatePaymentValidator to validate BookingID before Amount

--- BE/Validator/CreatePaymentValidator.cs
+++ BE/Validator/CreatePaymentValidator.cs
@@ -5,8 +5,11 @@ public class CreatePaymentValidator : AbstractValidator<CreatePaymentDTO>
 {
     public CreatePaymentValidator()
     {
-        RuleFor(x => x.Amount)
+        RuleFor(x => x.BookingID)
             .NotEmpty().WithMessage("Không được để trống.")
+            .GreaterThan(0).WithMessage("Số tiền phải lớn hơn 0.");
+
+        RuleFor(x => x.Amount)
             .GreaterThan(0).WithMessage("Số tiền phải lớn hơn 0.");
     }
 }
