4d10e4060d314dcbf4c5495ed9149536f0f57500
KarlDragon <giocodeproject@gmail.com>
2026-09-09 10:50:31 +0700

    Refactor PaymentService to calculate amount from booking service and initiate payment with GatewayPaymentRequest

--- BE/Services/Implementations/PaymentService.cs
+++ BE/Services/Implementations/PaymentService.cs
@@ -3,24 +3,31 @@ using BE.Services.Interfaces;
 using BE.Repositories.Interfaces;
 using BE.DTOs;
 using BE.Models;
+using BE.Infrastructure.Payments;
 public class PaymentService : IPaymentService
 {
     private readonly IPaymentRepository _paymentRepository;
     private readonly IPaymentAttempRepository _paymentAttempRepository;
-
+    private readonly IBookingService _bookingService;
+    private readonly MomoClient _momoClient;
     public PaymentService(IPaymentRepository paymentRepository,
-                          IPaymentAttempRepository paymentAttempRepository )
+                          IPaymentAttempRepository paymentAttempRepository,
+                          IBookingService bookingService,
+                          MomoClient momoClient)
     {
         _paymentRepository = paymentRepository;
         _paymentAttempRepository = paymentAttempRepository;
+        _bookingService = bookingService;
+        _momoClient = momoClient;
     }
 
     public async Task CreatePaymentAsync(CreatePaymentDTO createPaymentDTO)
     {
+        int amount = await _bookingService.CalculateTotalBookingPriceAsync(createPaymentDTO.BookingID);
         var newPayment = new Payment
         {
             BookingID = createPaymentDTO.BookingID,
-            Amount = createPaymentDTO.Amount,
+            Amount = amount,
             Status = PaymentStatus.Pending,
             CreatedAt = DateTime.UtcNow
         };
@@ -38,6 +45,17 @@ public class PaymentService : IPaymentService
             Status = PaymentAttemptStatus.Init,
             CreatedAt = DateTime.UtcNow
         };
+
+        var newGatewayPaymenyRequest = new GatewayPaymentRequest
+        {
+            RequestId = requestId,
+            Amount = amount,
+            OrderId = orderId,
+            OrderInfo = createPaymentDTO.OrderId,
+            ExtraData = ""
+        };
+        
         await _paymentAttempRepository.CreatePaymentAttemptAsync(newPaymentAttempt);
+        await _momoClient.InitiatePayment(newGatewayPaymenyRequest);
     }
 }
