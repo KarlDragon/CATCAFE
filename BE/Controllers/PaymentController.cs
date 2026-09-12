3a56b4f04546df81050937dfef9b819502164427
KarlDragon <giocodeproject@gmail.com>
2026-09-12 12:44:17 +0700

    Add PaymentController with API routing and controller attributes

---
+++ BE/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+namespace BE.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using BE.Services.Interfaces;
+using BE.Infrastructure.Payments;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class PaymentController : ControllerBase
+{
+    
+}
